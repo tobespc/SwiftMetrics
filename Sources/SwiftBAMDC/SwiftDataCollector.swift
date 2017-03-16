86f54d70b11f8600fe5d2699b0c1a394bf359e97
corbint@uk.ibm.com <corbint@uk.ibm.com>
2017-03-16 18:57:48 +0000

    add debug

--- Sources/SwiftBAMDC/SwiftDataCollector.swift
+++ Sources/SwiftBAMDC/SwiftDataCollector.swift
@@ -21,16 +21,16 @@ import HeliumLogger
 import LoggerAPI
 
 public class SwiftDataCollector {
-    
+
     public var monitor:SwiftMonitor
     public var swMetricInstance:SwiftMetrics
-    
+
     var logLevel : LoggerMessageType = .info
     var swiftDataCollectorInited = false
     var envData: [String:String]!
     var vcapAppDictionary : [String: Any]!
     var applicationName: String = ""
-    
+
     var formatter = DateFormatter()
     var cpuSampleTime: TimeInterval = 0
     var memSampleTime: TimeInterval = 0
@@ -39,15 +39,15 @@ public class SwiftDataCollector {
     var osResourceID: String!
     var origin: String!
     var tenant: String!
- 
-    
+
+
     let bamConfig = BMConfig.sharedInstance
-    
+
     public init(swiftMetricsInstance : SwiftMetrics) throws {
 
         self.logLevel = self.bamConfig.logLevel
         HeliumLogger.use(self.logLevel)
-        
+  print("[SwiftDataCollector] found it")      
         self.formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
         self.formatter.timeZone = TimeZone(abbreviation: "UTC") as TimeZone!
 
@@ -55,70 +55,70 @@ public class SwiftDataCollector {
         self.origin = self.bamConfig.dcId
         self.tenant = self.bamConfig.tenantId
         self.osResourceID = self.bamConfig.getResourceId(resName: "osID")
-        
+
         //self.swMetricInstance = try SwiftMetrics()
         self.swMetricInstance = swiftMetricsInstance
         self.monitor = swiftMetricsInstance.monitor()
-        
+
         monitor.on({ (_: InitData) in
             self.envInitandTopoRegister()
         })
-        
+
         _ = SwiftMetricsKitura(swiftMetricsInstance: swiftMetricsInstance)
-        
+
         Log.debug("SwiftDataCollector init: origin = \(origin!), appResourceID = \(appResourceID!), osResourceID = \(osResourceID!),  tenant = \(tenant!)")
-        
+
         monitor.on(sendCPUMetrics)
         monitor.on(sendMemMetrics)
         monitor.on(sendAARData)
-        
+
     }
-    
+
     func sendCPUMetrics(cpu: CPUData) {
-        
+
         let timeAsInterval: TimeInterval = Double(cpu.timeOfSample)/1000
         let dataDate = Date(timeIntervalSince1970: timeAsInterval)
         let dataTimeStamp = self.formatter.string(from: dataDate as Date)
-        
+
         if self.swiftDataCollectorInited {
             if ((self.cpuSampleTime == 0)||((timeAsInterval - self.cpuSampleTime) > 60)){
-                
+
                 self.cpuSampleTime = timeAsInterval
-                
+
                 let systemCPUMetrics : Dictionary<String,Any> = [
                     "timestamp": dataTimeStamp,
                     "resourceID": osResourceID,
                     "dimensions": ["name": "system"],
                     "metrics": ["system_cpuPercentUsed" : cpu.percentUsedBySystem]]
-                
+
                 Log.debug("systemCPUMetrics: \(systemCPUMetrics)")
-                
+
                 self.bamConfig.makeAPMRequest(perfData: systemCPUMetrics, postURL: bamConfig.metricURL)
-                
+
                 let appCPUMetrics : Dictionary<String,Any> = [
                     "timestamp": dataTimeStamp,
                     "resourceID": appResourceID,
                     "dimensions": ["name": self.applicationName],
                     "metrics": ["app_cpuPercentUsed" : cpu.percentUsedByApplication]]
-                
+
                 Log.debug("appCPUMetrics: \(appCPUMetrics)")
-                
+
                 self.bamConfig.makeAPMRequest(perfData: appCPUMetrics, postURL: bamConfig.metricURL)
             }
         }
     }
-    
+
     func sendMemMetrics(mem: MemData) {
-        
+
         let timeAsInterval: TimeInterval = Double(mem.timeOfSample)/1000
         let dataDate = Date(timeIntervalSince1970: timeAsInterval)
         let dataTimeStamp = self.formatter.string(from: dataDate as Date)
-        
+
         if self.swiftDataCollectorInited {
             if ((self.memSampleTime == 0)||((timeAsInterval - self.memSampleTime) > 60)){
-                
+
                 self.memSampleTime = timeAsInterval
-                
+
                 let systemMemMetrics : Dictionary<String,Any> = [
                     "timestamp": dataTimeStamp,
                     "resourceID": osResourceID,
@@ -127,11 +127,11 @@ public class SwiftDataCollector {
                                 "system_totalMemoryUsed" : mem.totalRAMUsed,
                                 "system_totalMemoryFree" : mem.totalRAMFree]
                 ]
-                
+
                 Log.debug("systemMemMetrics: \(systemMemMetrics)")
-                
+
                 self.bamConfig.makeAPMRequest(perfData: systemMemMetrics, postURL: bamConfig.metricURL)
-                
+
                 let appMemMetrics : Dictionary<String,Any> = [
                     "timestamp": dataTimeStamp,
                     "resourceID": appResourceID,
@@ -139,40 +139,40 @@ public class SwiftDataCollector {
                     "metrics": ["app_memAddressSpaceSize" : mem.applicationAddressSpaceSize,
                                 "app_memPrivateSize" : mem.applicationPrivateSize,
                                 "app_memUsed" : mem.applicationRAMUsed]]
-                
+
                 Log.debug("appMemMetrics: \(appMemMetrics)")
-                
+
                 self.bamConfig.makeAPMRequest(perfData: appMemMetrics, postURL: bamConfig.metricURL)
             }
         }
     }
-    
+
     func sendAARData(httpData: HTTPData) {
-        
+
         var processID: String = ""
         var serverName: String = ""
         var serverAddress: String = ""
-        
+
         let startTimeInterval: TimeInterval = Double(httpData.timeOfRequest)/1000
         let finishTimeInterval: TimeInterval = (Double(httpData.timeOfRequest) + Double (httpData.duration))/1000
         let startTime = Date(timeIntervalSince1970: startTimeInterval)
         let startTimeStamp = self.formatter.string(from: startTime as Date)
         let finishTime = Date(timeIntervalSince1970: finishTimeInterval)
         let finishTimeStamp = self.formatter.string(from: finishTime as Date)
-        
+
         if self.swiftDataCollectorInited {
             if self.envData != nil{
                 processID = self.envData["pid"]!
                 serverName = self.envData["environment.HOSTNAME"]!
                 serverAddress = self.envData["environment.CF_INSTANCE_ADDR"]!
             }
-            
+
             let aarMetrics : Dictionary<String,Any> = [
                 "status": "\(httpData.statusCode!)",
                 "responseTime": httpData.duration]
-            
+
             let documentID: String = NSUUID().uuidString
-            
+
             let aarProperties : Dictionary<String,Any> = [
                 "documentType": "/AAR/MIDDLEWARE/SWIFT",
                 "softwareServerType": "http://open-services.net/ns/crtv#Swift",
@@ -189,37 +189,37 @@ public class SwiftDataCollector {
                 "requestName": httpData.url,
                 "originID": origin,
                 "tenantID": tenant]
-            
+
             let aarData: Dictionary<String,Any> = [
                 "metrics": aarMetrics,
                 "properties": aarProperties]
-            
+
             Log.debug("aarData: \(aarData)")
-            
+
             self.bamConfig.makeAPMRequest(perfData: aarData, postURL: bamConfig.aarURL)
         }
     }
 
     func envInitandTopoRegister() -> Void {
-        
+
         self.envData = self.monitor.getEnvironmentData()
         Log.debug("envData: \(envData!)")
-        
+
         let currentTime = Date()
         let utcTimeZoneStr = self.formatter.string(from: currentTime as Date)
-        
+
         if !self.envData["environment.VCAP_APPLICATION"]!.isEmpty{
             let data = self.envData["environment.VCAP_APPLICATION"]!.data(using: String.Encoding.utf8)! as Data
             self.vcapAppDictionary = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String: Any]
         }
         Log.debug("vcapAppDictionary: \(vcapAppDictionary!)")
-        
+
         if self.vcapAppDictionary != nil{
             self.applicationName = self.vcapAppDictionary["application_name"] as! String
         }
-        
+
         self.swiftDataCollectorInited = true
-        
+
         var swiftAppResource : Dictionary<String,Any> = [
             "uniqueId": appResourceID,
             "_status" : ["status": "normal"],
@@ -230,13 +230,13 @@ public class SwiftDataCollector {
             "startedTime": utcTimeZoneStr,
             "references": ["runsOn":[["to": osResourceID!]]]
           ]
-        
+
         for (key, value) in self.vcapAppDictionary {
             //print(" \(key) = \(value)")
             swiftAppResource["\(key)"] = value
         }
         Log.debug("swiftAppResource: \(swiftAppResource)")
-        
+
         let hostName = self.envData["environment.HOSTNAME"]!
         let version = self.envData["os.version"]! as String
         let osName = self.envData["os.name"]! as String
@@ -257,9 +257,9 @@ public class SwiftDataCollector {
             "entityTypes": ["compute"]
         ]
         Log.debug("swiftOSResource: \(swiftOSResource)")
-        
+
         let swiftDCName : String = "SwiftDC-"+self.applicationName
-        
+
         let swiftProvider : Dictionary<String,Any> = [
             "uniqueId": self.origin,
             "displayLabel": swiftDCName,
@@ -271,7 +271,7 @@ public class SwiftDataCollector {
             "monitoringLevel": "L1"
         ]
         Log.debug("swiftProvider: \(swiftProvider)")
-        
+
         self.bamConfig.makeAPMRequest(perfData: swiftProvider, postURL: bamConfig.providerURL)
         self.bamConfig.makeAPMRequest(perfData: swiftOSResource, postURL: bamConfig.topoURL)
         self.bamConfig.makeAPMRequest(perfData: swiftAppResource, postURL: bamConfig.topoURL)
