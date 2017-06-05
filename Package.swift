30be3c76e4637b09ade5b7cf62141e62f548e8e6
corbint@uk.ibm.com <corbint@uk.ibm.com>
2017-06-05 13:54:47 +0100

    update kitura level

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
       Target(name: "hcapiplugin", dependencies: [.Target(name: "agentcore")])
    ],
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
     .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 7),
     .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
     .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1)
@@ -63,4 +63,3 @@ let package = Package(
               excludePortDir
    ]
 )
-
